194e40a54a6a1031c671e047c93251762704c6ea
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2025-04-25 16:17:58 +0100

    UPDATE: Code updates and whatnot

--- index.js
+++ index.js
@@ -10,40 +10,38 @@ const fileSteamURL = join(__dirname, "/profiles/steam_urls.txt");
 const isSteamProfileURL = /(?:https?:\/\/)?steamcommunity\.com\/(?:profiles|id)\/[a-zA-Z0-9]+/;
 
 // --| Check if the file is empty first
-lineReader.open(fileSteamURL, (err, reader) =>
-{
-    if(!reader.hasNextLine())
-    {
+lineReader.open(fileSteamURL, (err, reader) => {
+    if (err) {
+        console.error(`❌ Error opening file: ${fileSteamURL}`, err);
+        return;
+    }
+
+    if (!reader.hasNextLine()) {
         console.log(`❌ \x1b[31mStopped!\x1b[0m \x1b[33m${fileSteamURL}\x1b[0m does not contain any URL's!`);
+        return;
     }
 });
 
 // --| Read each line
-lineReader.eachLine(fileSteamURL, (line, last) =>
-{
+lineReader.eachLine(fileSteamURL, (line, last) => {
     // --| If the URL does not match the Steam Profile format, skip it.
-    if(!line.match(isSteamProfileURL))
-    {
+    if(!line.match(isSteamProfileURL)) {
         console.log(`♻️ Skipped \x1b[33m${line}\x1b[0m because is not a valid Steam Profile URL !`);
-
         return;
     }
 
     // --| Get each line
-    axios.get(line).then((response) =>
-    {
+    axios.get(line).then((response) => {
         const $ = cheerio.load(response.data);
         const checkVAC = $(".profile_ban").text().trim();
 
         // --| Check if profile is VAC banned
-        if(checkVAC.includes("Multiple VAC bans on record") || checkVAC.includes("VAC ban on record"))
-        {
+        if(checkVAC.includes("Multiple VAC bans on record") || checkVAC.includes("VAC ban on record")) {
             console.log(`🚨 Profile URL: \x1b[34m${line}\x1b[0m is \x1b[31mVAC banned\x1b[0m.`);
         }
 
         // --| Usually the user is VAC banned
-        else
-        {
+        else {
             console.log(`✅ Profile URL: \x1b[34m${line}\x1b[0m is \x1b[32mclean\x1b[0m.`);
         }
     });
